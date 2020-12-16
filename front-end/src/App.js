d2d72667116d2b0ec3321a6031de60dde0e5f3d3
Maharsh <gmaharsh@yahoo.com>
2020-12-15 21:48:11 -0600

    Added Order Path and React Router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Header from './Componets/Header/Header';
 import Home from './Componets/Home/Home';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Order from './Componets/Order/Order';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <h1>This is checkout Page</h1>
+          </Route>
+          <Route path="/chicago/:restaurent">
+            <Order />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
